c0adbbd2f34171a4ea64b62abdb0687f50389120
Amr Suliman <amrelgohary573@gmail.com>
2026-09-17 00:23:22 +0300

    Add project files.

---
+++ AssignmentOneOOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentOneOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
